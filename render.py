8a377f58c0b30a457b215bef49a091d6ed2d40a2
Matt Thornback <matt.thornback@gmail.com>
2024-06-02 17:18:08 -0400

    feat: add arguments to render

--- render.py
+++ render.py
@@ -1,17 +1,34 @@
-import yaml
-from jinja2 import Environment, FileSystemLoader
+#!/usr/bin/env python
 
-# Load YAML data
-with open('data.yaml', 'r') as file:
+import yaml
+import jinja2
+import argparse
+import os
+
+# Set up command-line argument parsing
+parser = argparse.ArgumentParser(description='Generate LaTeX document from template and YAML data.')
+parser.add_argument('yaml_file', type=str, help='The YAML file containing the data')
+parser.add_argument('--template', type=str, default='resume.j2', help='The Jinja2 template file (default: resume.j2)')
+
+args = parser.parse_args()
+
+# Derive the output filename from the input YAML filename
+yaml_filename = os.path.basename(args.yaml_file)
+output_filename = os.path.splitext(yaml_filename)[0] + '.tex'
+
+# Read YAML data from the specified file
+with open(args.yaml_file, 'r') as file:
     data = yaml.safe_load(file)
 
-# Load Jinja environment
-env = Environment(loader=FileSystemLoader('.'))
-template = env.get_template('resume.j2')
+# Load the Jinja2 template
+with open(args.template, 'r') as file:
+    template = jinja2.Template(file.read())
 
-# Render template with data
-rendered_tex = template.render(data)
+# Render the template with data
+output = template.render(data)
 
-# Write rendered LaTeX to file
-with open('output.tex', 'w') as file:
-    file.write(rendered_tex)
+# Write the output to the derived LaTeX file
+with open(output_filename, 'w') as file:
+    file.write(output)
+
+print(f"Rendered LaTeX document has been saved to {output_filename}")
